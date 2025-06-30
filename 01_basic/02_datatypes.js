614df998d1a9f10a9bddd0d7a5d80af9ab8033c9
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-06-30 12:36:19 +0000

    learned about datatypes ,type of null and undefined

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Hitesh")
+
+let name = "garvit"
+let age = 21
+let isLogged = false
+
+// null is type stand alone value //means woh khaali hai 
+// undefined  is that value abhi defined nhi hai
+
+// objects
+ console.log(typeof null); //iska type object hai
+ console.log(typeof undefined); // iska type undefined hee hai
+ 
+ 
+ 
