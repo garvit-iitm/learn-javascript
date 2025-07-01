a9e551e6f21f092706dc3f0a8acae2c0fc82bd51
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-01 20:04:39 +0000

    learned about stack and heap memory

--- 01_basic/primitive_nonprimitive.js
+++ 01_basic/primitive_nonprimitive.js
@@ -1,5 +1,7 @@
 // the following code is notes from my course on JavaScript learining
 
+const { memo } = require("react");
+
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
@@ -37,3 +39,38 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//---------------------------------learning stack and heap memory-----------------------------
+
+// Stack memory is used for primitive datatypes
+// Heap memory is used for non-primitive datatypes
+
+//----------------------------------example 1 for non primitive type
+
+let myDetails = {
+    name: "garvit",
+    age: 22,
+    address: "bareilly"
+}
+
+
+let myDetails2 = myDetails; // yeh same jageh par point kar rahe hain or yeh reference type hai
+
+myDetails2.name = "garvit kumar"; // yeh dono same jageh par point kar rahe hain isliye dono ka naam change ho jayega
+console.log(myDetails.name); // garvit kumar
+console.log(myDetails2.name); // garvit kumar
+
+//---------------------------------example 2 for primitive type
+
+let myName = "garvit";
+let myName2 = "bhatnagar"; // yeh alag alag jageh par point kar rahe hain or yeh primitive type hai
+
+myName2 = myName; // yeh ab myName ki value ko copy kar raha hai or yeh alag alag jageh par point kar rahe hain
+myName2 = "garvit singh bhatnagar"; // yeh ab myName ki value ko change nahi karega
+
+
+console.log(myName); // garvit
+console.log(myName2); // garvit singh bhatnagar
+
