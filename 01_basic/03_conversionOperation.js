fdb6d2d81204392ae1f71ead93792ab88091975d
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-01 15:35:48 +0000

    correcting errors in code

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -34,9 +34,9 @@ console.log(typeof stringNumber)
 
 
 
-log(1 + "2");
-console. log("1" + 2 + 2);
-console. log(1 + 2 +"2");
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 +"2");
 
 console.log(3+4*5% 3);
 
@@ -44,8 +44,8 @@ console.log(3+4*5% 3);
 //or agar integer first hai toh additon operation ho jaega orr frr string kee tareh add hoga
 
 
-console. log(+true); // this will print 1
+console.log(+true); // this will print 1
 // console. log(true+); // this will show an unexepected expression and hence error
-console. log (+""); // this will print 0
+console.log(+""); // this will print 0
 
 //also learned about prefix and postfix operations from mdm js documentation
