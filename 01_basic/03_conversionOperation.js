df16ee83650fd6c8bbf6b87c97ec3f445aa0d99c
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-01 15:34:05 +0000

    learned about operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -26,3 +26,26 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+
+
+
+
+//--------------------------------learning about operations-------------------------------------------
+
+
+
+log(1 + "2");
+console. log("1" + 2 + 2);
+console. log(1 + 2 +"2");
+
+console.log(3+4*5% 3);
+
+//agar string first hai toh saare addition string samajh kar honge 
+//or agar integer first hai toh additon operation ho jaega orr frr string kee tareh add hoga
+
+
+console. log(+true); // this will print 1
+// console. log(true+); // this will show an unexepected expression and hence error
+console. log (+""); // this will print 0
+
+//also learned about prefix and postfix operations from mdm js documentation
