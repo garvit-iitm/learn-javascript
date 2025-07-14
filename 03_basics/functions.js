01c2f0b975a47729f0d1fa779fb55286ff775530
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-14 18:25:16 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,1 +1,11 @@
 // this is my learning for JavaScript functions
+
+function sayMyName ( ) {
+console. log ("G") ;
+console. log ("A");
+console. log ("R");
+console. log ("V");
+console. log ("I");
+console. log ("T");
+
+sayMyName();
