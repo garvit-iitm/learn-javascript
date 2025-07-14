8f3fc2604b397825cd57215bd907473d70c909bc
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-14 18:40:05 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,11 +1,31 @@
 // this is my learning for JavaScript functions
 
-function sayMyName ( ) {
+function sayMyName () {
 console. log ("G") ;
 console. log ("A");
 console. log ("R");
 console. log ("V");
 console. log ("I");
 console. log ("T");
+}
+sayMyName () ;
 
-sayMyName();
+function addTwoNumbers (number1, number2) {
+
+let result = number1 + number2
+return result
+}
+const result = addTwoNumbers (3, 5)
+
+console. log("Result: ", result) ; 
+
+function loginUserMessage (username) {
+if (!username) {
+console. log ("PLease enter a username") ;
+return
+}
+
+return `${username} just logged in`
+}
+console. log (loginUserMessage ("garvit") )
+
