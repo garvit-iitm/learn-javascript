7182b67ed0738abb55449669aad55567c0d79713
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-15 18:12:32 +0000

    learned about scope and hosting in javaScript

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -22,3 +22,17 @@ console. log(a);// Accessing outer 'a' ........output:300
 //just remember that the scope is different in code encvironments like browser and node.js
 //in browser, the global scope is the window object
 //in node.js, the global scope is the global object
+
+//=========================================================================================================================
+
+
+//learning hosting in JavaScript
+//hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of
+//their containing scope during the compile phase, before the code is executed.
+
+function hoistingExample() {
+    console.log(myVar); // undefined, because of hoisting
+    let myVar = "I am hoisted";
+    console.log(myVar); // "I am hoisted"
+}
+hoistingExample();      
