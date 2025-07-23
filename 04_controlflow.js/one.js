33a15f5eea671c58808ce5541785d42018841322
garvit-iitm <23f1001875@ds.study.iitm.ac.in>
2025-07-23 18:19:47 +0000

    learned about control statements

---
+++ 04_controlflow.js/one.js
@@ -0,0 +1,13 @@
+// if-else
+
+isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+  console.log("User is logged in");
+}
+else {
+  console.log("User is not logged in");
+}
+
+// == it checks two values for equality
+// === it checks two values for equality and same data type
